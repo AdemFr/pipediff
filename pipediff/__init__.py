612f332afb6ac671be88591fd8a3785abc56ab4e
Adem Frenk <adem.frenk@gmail.com>
2020-11-25 00:37:07 +0100

    Add simple diff class

--- pipediff/__init__.py
+++ pipediff/__init__.py
@@ -1,5 +1,8 @@
 """Top-level package for pipediff."""
+from .pipediff import PipeDiff
+
+__all__ = ("PipeDiff",)
 
 __author__ = """Adem Frenk"""
-__email__ = 'adem.frenk@gmail.com'
-__version__ = '0.1.0'
+__email__ = "adem.frenk@gmail.com"
+__version__ = "0.1.0"
--- pipediff/pipediff.py
+++ pipediff/pipediff.py
@@ -1,1 +1,47 @@
-"""Main module."""
+class IndexDiff:
+    def __init__(self, df_1, df_2):
+        self.df_1 = df_1
+        self.df_2 = df_2
+
+    @property
+    def left(self):
+        return self.df_1.index.difference(self.df_2.index)
+
+    @property
+    def right(self):
+        return self.df_2.index.difference(self.df_1.index)
+
+    @property
+    def intersection(self):
+        return self.df_1.index.intersection(self.df_2.index)
+
+
+class ColumnDiff:
+    def __init__(self, df_1, df_2):
+        self.df_1 = df_1
+        self.df_2 = df_2
+
+    @property
+    def left(self):
+        return self.df_1.columns.difference(self.df_2.columns)
+
+    @property
+    def right(self):
+        return self.df_2.columns.difference(self.df_1.columns)
+
+    @property
+    def intersection(self):
+        return self.df_1.columns.intersection(self.df_2.columns)
+
+
+class PipeDiff:
+    def __init__(self, df_1, df_2):
+        self.df_1 = df_1
+        self.df_2 = df_2
+        self.idx = IndexDiff(self.df_1, self.df_2)
+        self.col = ColumnDiff(self.df_1, self.df_2)
+
+    def compare_intersection(self, *args, **kwargs):
+        idx, cols = self.idx.intersection, self.col.intersection
+
+        return self.df_1.loc[idx, cols].compare(self.df_2.loc[idx, cols], *args, **kwargs)
