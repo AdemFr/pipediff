5cb0d58129749b050142e33e10feaafd03601427
Adem Frenk <adem.frenk@gmail.com>
2020-12-05 23:06:20 +0100

    fix docs module path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,10 @@
 #
 import os
 import sys
+
+sys.path.insert(0, os.path.abspath("../../"))
 import pipediff
 
-sys.path.insert(0, os.path.abspath("../../../"))
 
 # -- Project information -----------------------------------------------------
 
