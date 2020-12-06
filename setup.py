3a32627b8c26a6cd5d270b47a40058dd614ba7ec
Adem Frenk <adem.frenk@gmail.com>
2020-12-06 12:24:56 +0100

    Update python versions

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
     ],
     description="A python packages to make functional pandas pipelines more transparent.",
     install_requires=requirements,
