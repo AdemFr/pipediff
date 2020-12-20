ec3e4572ef72a10b685482e114ee90465e9f2b44
Adem Frenk <adem.frenk@gmail.com>
2020-12-20 21:41:34 +0100

    Add ColumnDiff.compare

--- pipediff/column_diff.py
+++ pipediff/column_diff.py
@@ -1,4 +1,14 @@
+from typing import Union
+
 import pandas as pd
+
+COLUMN_AGG = {
+    "nan": lambda s: s.isna().sum(),
+    "count": lambda s: s.count(),
+    "max": lambda s: s.max(),
+    "min": lambda s: s.min(),
+    "mean": lambda s: s.mean(),
+}
 
 
 class ColumnDiff:
@@ -33,3 +43,30 @@ class ColumnDiff:
     def intersection(self) -> pd.Index:
         """Gets all columns that df_1 and df_2 have in common."""
         return self.df_1.columns.intersection(self.df_2.columns)
+
+    def compare(self, column_name: str, aggregation_function: Union[str, callable]) -> pd.Series:
+        """Aggregate a column and compare the result for df_1 and df_2.
+
+        Args:
+            column_name (str): The name of the column to compare.
+            aggregation_function (Union[str, callable]): Either callable or one of these shorthands
+                "nan", "max", "min", "mean"
+        """
+        if aggregation_function in COLUMN_AGG.keys():
+            agg_func = COLUMN_AGG[aggregation_function]
+        elif callable(aggregation_function):
+            agg_func = aggregation_function
+        else:
+            raise ValueError(
+                f"Invalid value for 'aggregation_function'."
+                f" Either has to be a callable or one of these valid shorthands: {list(COLUMN_AGG.keys())}."
+            )
+
+        values = {}
+        # Calculate aggregate statistics
+        if column_name in self.df_1.columns:
+            values["left"] = agg_func(self.df_1.loc[:, column_name])
+        if column_name in self.df_2.columns:
+            values["right"] = agg_func(self.df_2.loc[:, column_name])
+
+        return pd.Series(values)
--- pipediff/frame_diff.py
+++ pipediff/frame_diff.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 from pipediff import ColumnDiff, IndexDiff
 
 
