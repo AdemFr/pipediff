86d8cc0ef07c4d9596fa384f9fe109574a383048
Adem Frenk <adem.frenk@gmail.com>
2020-12-27 18:53:33 +0100

    track_frame and compare for DiffTracker

--- pipediff/track.py
+++ pipediff/track.py
@@ -1,9 +1,11 @@
 from functools import wraps
+from typing import Union, Optional
 
 import pandas as pd
 from typing import Any, List
 
 from pipediff import FrameDiff
+from pipediff.constants import MULTI_INDEX_KEYS
 
 
 def agg_func_to_list(
@@ -82,9 +84,59 @@ class DiffTracker:
         self.deactivate = deactivate
 
         self.diffs = {}
+        self.column_stats = []
         # TODO
         # build dataframe out of self.diffs
         # self.number_of_diffs
+
+    def track_frame(
+        self, df: pd.DataFrame, agg_func: Union[callable, str, list, dict], *args, **kwargs
+    ) -> pd.DataFrame:
+        """Record all relevant statistics and properties of a dataframe and add them to a collection."""
+
+        # Only take relevant column names into account.
+        columns = df.columns.intersection(self.column_names) if self.column_names is not None else df.columns
+
+        agg_func = agg_func_to_list(agg_func, columns)
+        agg_col = df.agg(agg_func, *args, **kwargs)
+        agg_col = agg_col.reindex(columns, axis=1)  # Add all existing columns to the stats
+        self.column_stats.append(agg_col)
+
+        # Calculate aggregate stats based on DiffTracker attributes
+        # Calculate column stats
+        # Calculate index stats
+        # Keep copy of sliced frame if needed
+
+        # Add stats and copy of df to the list of stats
+        return agg_col
+
+    def compare(self, ids: tuple = None):
+        """Compare a pair of statistics and present them in a multi index dataframe."""
+
+        # If ids are given, compare those two ids explicitly
+        if len(self.column_stats) < 2:
+            raise IndexError("There are at least two recorded stats needed for a comparison!")
+        _ids = (-2, -1) if ids is None else ids  # Take the last two as default
+        stats_1, stats_2 = self.column_stats[_ids[0]], self.column_stats[_ids[1]]
+        stats_comp = pd.concat([stats_1, stats_2], axis=1, keys=MULTI_INDEX_KEYS)
+        stats_comp = stats_comp.swaplevel(axis=1).sort_index(axis=1)  # Reorder multi index with column labels on top
+
+        col_union = stats_1.columns.union(stats_2.columns)
+        # Calculate binary flags for all columns in the union.
+        cols_1_exist = col_union.isin(stats_1.columns).reshape(-1, len(col_union))
+        cols_2_exist = col_union.isin(stats_2.columns).reshape(-1, len(col_union))
+
+        # Format values as one data frame with a multi index.
+        cols_1_exist = pd.DataFrame(cols_1_exist, index=["exists"], columns=col_union)
+        cols_2_exist = pd.DataFrame(cols_2_exist, index=["exists"], columns=col_union)
+
+        exists_comp = pd.concat([cols_1_exist, cols_2_exist], axis=1, keys=MULTI_INDEX_KEYS)
+        exists_comp = exists_comp.swaplevel(axis=1).sort_index(axis=1)  # Reorder multi index with column labels on top
+
+        comp = pd.concat([exists_comp, stats_comp])
+        comp = comp.T.astype({"exists": bool}).T  # Convert the exists row into boolean values for easier display
+
+        return comp
 
     def track(self, **kwargs) -> callable:
         """Returns a decorator to be used for tracking the input and output of a function.
