97712b5c73d7fa20d124ad095ddc50b00436e3e6
Adem Frenk <adem.frenk@gmail.com>
2021-02-28 18:55:24 +0100

    FrameLogs

--- pipediff/diff_tracker.py
+++ pipediff/diff_tracker.py
@@ -1,27 +1,55 @@
+from collections import OrderedDict
+from itertools import islice
+from typing import Any, Union
+
 import pandas as pd
 
 
+class FrameLogs(OrderedDict):
+    """An OrderedDict, which supports slicing, integer access and some custom functionality."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Overwritten, to initialise additional parameters that should be tracked."""
+        # It is important to assign _assignment_counter before super().__init__ because the instantiation might
+        # call __setitem__ and will result in not finding this attribute.
+        self._assignment_counter = 0
+        super().__init__(*args, **kwargs)
+
+    def __setitem__(self, *args, **kwargs) -> None:
+        """Overwrites the original version, to be able to count assignments."""
+        if not isinstance(args[0], str):
+            raise ValueError("Keys should always be a string to enable unambiguous integer access, e.g. logs[0]")
+        super().__setitem__(*args, **kwargs)
+        self._assignment_counter += 1
+
+    def __getitem__(self, k: Union[slice]) -> Any:
+        """Overwrites the original version, to be able to get a slice with frame_logs[1:3]."""
+        if isinstance(k, slice):
+            return FrameLogs(islice(self.items(), k.start, k.stop, k.step))
+        elif isinstance(k, int):
+            # Take the kth key and return the value
+            key = list(self.keys())[k]
+            return self[key]
+        else:
+            return super().__getitem__(k)
+
+    def append(self, value: Any, key: str = None) -> str:
+        """Append new entry. If key is not given a new one will be created with the prefix."""
+        if key is not None and key in self:
+            raise KeyError(f"Key '{key}' already exists!")
+        elif key is None:
+            self[f"df_{self._assignment_counter}"] = value
+        else:
+            self[key] = value
+
+
 class DiffTracker:
-    def __init__(self) -> None:
-        self.frame_logs = {}
-        self._frame_logs_counter = 0
+    def __init__(self, log_nans: bool = True) -> None:
+        self.frame_logs = FrameLogs()
+        self.log_nans = log_nans
 
-    def log_frame(self, df: pd.DataFrame, name: str = None) -> None:
-        name = self._get_unique_frame_name(name)
-        self._frame_logs_counter += 1
-
-        self.frame_logs[name] = "asd"
+    def log_frame(self, df: pd.DataFrame, key: str = None) -> None:
+        self.frame_logs.append(value=df, key=key)
 
     def reset(self) -> None:
-        self.frame_logs = {}
-        self._frame_logs_counter = 0
-
-    def _get_unique_frame_name(self, name: str = None) -> str:
-        """If a name is given, checks if it exists, otherwise create a new one."""
-
-        if name in self.frame_logs:
-            raise KeyError(f"Key '{name}' already exists!")
-        elif name is None:
-            return f"df_{self._frame_logs_counter}"
-        else:
-            return name
+        self.frame_logs = FrameLogs()
--- tests/test_pipediff.py
+++ tests/test_pipediff.py
@@ -1,3 +1,4 @@
+from typing import KeysView
 import pandas as pd
 import pytest
 
@@ -10,9 +11,7 @@ def tracker() -> DiffTracker:
 
 
 def test_default_attributes(tracker: DiffTracker) -> None:
-    fl = tracker.frame_logs
-
-    assert fl == {}
+    assert len(tracker.frame_logs) == 0
 
 
 def test_log_empty_frame(tracker: DiffTracker) -> None:
@@ -21,12 +20,15 @@ def test_log_empty_frame(tracker: DiffTracker) -> None:
 
     assert len(fl) == 1
     assert fl.get("df_0") is not None
+    assert fl[0] is not None
+    assert fl[-1] is not None
 
 
 def test_log_frame_with_name(tracker: DiffTracker) -> None:
-    tracker.log_frame(pd.DataFrame(), name="my_frame")
-
+    tracker.log_frame(pd.DataFrame(), key="my_frame")
     assert tracker.frame_logs.get("my_frame") is not None
+    with pytest.raises(KeyError):
+        tracker.log_frame(pd.DataFrame(), key="my_frame")
 
 
 def test_reset_tracker_is_empty(tracker: DiffTracker) -> None:
@@ -42,6 +44,12 @@ def test_log_multiple_frames(tracker: DiffTracker) -> None:
 
     assert len(tracker.frame_logs) == 2
 
-    tracker.log_frame(pd.DataFrame(), name="my_frame")
+    tracker.log_frame(pd.DataFrame(), key="my_frame")
     with pytest.raises(KeyError):
-        tracker.log_frame(pd.DataFrame(), name="my_frame")
+        tracker.log_frame(pd.DataFrame(), key="my_frame")
+
+
+def test_log_nans_of_emtpy_frame() -> None:
+    tracker = DiffTracker(log_nans=True)
+    tracker.log_frame(pd.DataFrame())
+
