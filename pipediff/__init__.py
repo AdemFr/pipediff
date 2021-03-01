6146eeb3658122d21e8d07896ffb6cb6104d23ca
Adem Frenk <adem.frenk@gmail.com>
2021-03-01 01:12:15 +0100

    cleanup

--- pipediff/__init__.py
+++ pipediff/__init__.py
@@ -1,10 +1,7 @@
 """Top-level package for pipediff."""
-from pipediff.column_diff import ColumnDiff
-from pipediff.index_diff import IndexDiff
-from pipediff.frame_diff import FrameDiff
 from pipediff.diff_tracker import DiffTracker
 
-__all__ = ("ColumnDiff", "IndexDiff", "FrameDiff", "DiffTracker")
+__all__ = ("DiffTracker",)
 
 __author__ = """Adem Frenk"""
 __email__ = "adem.frenk@gmail.com"
